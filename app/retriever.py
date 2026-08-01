f01a81d80e643bc82f216289d6ca76301f2f6302
Muhammad Afzal <afzaal.lucky786@gmail.com>
2026-08-02 01:11:00 +0400

    refactor retriever file

--- app/retriever.py
+++ app/retriever.py
@@ -1,21 +1,3 @@
-# from app.qdrant_client import get_client
-# from app.ollama_client import embed
-# from app.config import COLLECTION_NAME
-
-# def search(query: str, top_k: int = 5):
-#     qdrant = get_client()
-
-#     query_vector = embed(query)
-
-#     results = qdrant.search(
-#         collection_name=COLLECTION_NAME,
-#         query_vector=query_vector,
-#         limit=top_k
-#     )
-
-#     return [r.payload["text"] for r in results]
-
-
 from app.qdrant_client import get_client
 from app.ollama_client import embed
 from app.config import COLLECTION_NAME
